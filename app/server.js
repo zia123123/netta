89106e4c284edaf9195f3c10c9cfce50e6b17798
zia123123 <ziamuhamad@gmail.com>
2023-06-02 10:18:48 +0700

    add env

--- app/server.js
+++ app/server.js
@@ -31,7 +31,7 @@ app.use(function(req, res, next) {
 app.use(cors(corsOpts));
 
 // Settings
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 
 // Middlewares
 app.use(express.json());
